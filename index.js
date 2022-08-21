768a487e0514656854019650f7e42a417610f8a6
Sugut <sugutvictorine@gmail.com>
2022-08-21 22:34:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers,name){
+ return drivers.filter( input =>(
+        input.toLowerCase() === name.toLowerCase()
+ ));
+}
+function fuzzyMatch(drivers,name){
+        return drivers.filter (input =>(
+        input.slice(0,name.length)===name
+        ));      
+}
+function matchName(drivers, name){
+        return drivers.filter(input => (
+                input.name === name
+        ));
+}
